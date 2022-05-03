4b7dec47c6431cbc9f51796c4cd7aee285b9ca42
JEAN YANN AKA <akajeanyann@gmail.com>
2022-05-03 16:30:19 +0000

    create

--- common/models/equipement-has-hebergement.js
+++ common/models/equipement-has-hebergement.js
@@ -4,25 +4,25 @@ const equipement = require("./equipement");
 
 module.exports = function(Equipementhashebergement) {
 
-    Equipementhashebergement.createequipment = function (req ,cb) {
+    Equipementhashebergement.createequipment = function (req, id, cb) {
         
-        const Equipement = req.body.equipement;
-        const Hebergement = req.body.hebergement;
+        const Equipement = Equipementhashebergement.app.models.equipement;
+        const Hebergement =Equipementhashebergement.app.models.hebergement;
 
-        var infoequip = Equipementhashebergement.app.models.infoequip;
+        var infoequip = req.body.infoequip;
 
         Equipement.findOne({
             where:{
-                id: infoequip.id
+                id: id
             }
         },(err, equipement) =>{
             console.log(equipement);
 
-            Equipement.destroyAll();
-            console.log(Equipement);
+            // Equipement.destroyAll();
+            // console.log(Equipement);
 
-            Equipement.create(infoequip)
-            console.log(Equipement);
+            // Equipement.create(infoequip)
+            // console.log(Equipement);
 
         })
 
@@ -34,8 +34,9 @@ module.exports = function(Equipementhashebergement) {
     {
         accepts: [
             { arg: 'req', type: 'object', 'http': {source: 'req'}},
+            { arg: 'id', type:'string', required: true}
         ],
-        http: { path: '/createequipment', verb: 'post'},
+        http: { path: '/:id/createequipment', verb: 'post'},
         returns : { type: 'object', root: true } 
     });
 };
