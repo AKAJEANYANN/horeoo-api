e3179ac5eea5d0e8e98d71cd61a28c03adfdd2ed
JEAN YANN AKA <akajeanyann@gmail.com>
2022-05-03 15:55:14 +0000

    active hebergement

--- common/models/equipement-has-hebergement.js
+++ common/models/equipement-has-hebergement.js
@@ -2,4 +2,25 @@
 
 module.exports = function(Equipementhashebergement) {
 
+    Equipementhashebergement.createequipment = function (req ,cb) {
+        
+        const Equipement = req.body.equipement;
+        const Hebergement = req.body.hebergement;
+
+        var infoequip = Equipementhashebergement.app.models.infoequip;
+
+        
+
+    }
+    
+    
+    
+    Provider.remoteMethod('createequipment',
+    {
+        accepts: [
+            { arg: 'req', type: 'object', 'http': {source: 'req'}},
+        ],
+        http: { path: '/createequipment', verb: 'post'},
+        returns : { type: 'object', root: true } 
+    });
 };
