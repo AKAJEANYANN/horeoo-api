491d083ce40bf72334bf209b53f3485e48ee547c
JEAN YANN AKA <akajeanyann@gmail.com>
2022-05-25 16:43:30 +0000

    hebergement random

--- common/models/hebergement.js
+++ common/models/hebergement.js
@@ -14,6 +14,7 @@ module.exports = function(Hebergement) {
         // });
         Hebergement.find({
             limit: limit,
+            include:"offre",
             where:{
                 typeHebergementId: idTypeHebergement
             }
