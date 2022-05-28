8dace7d0bd4a253421e9e9d07e1b0b9e7aed015c
JEAN YANN AKA <akajeanyann@gmail.com>
2022-05-28 17:32:44 +0000

    hebergementhascutomer liked

--- common/models/hebergement-has-customer.js
+++ common/models/hebergement-has-customer.js
@@ -1,5 +1,43 @@
 'use strict';
 
 module.exports = function(Hebergementhascustomer) {
+    Hebergementhascustomer.liked = function (customerId, cb) {
+
+        const Offre = Hebergementhascustomer.app.models.offre;
+
+        Hebergementhascustomer.find({
+            where:{
+                customerId: customerId
+            },
+            include:{
+                relation:'hebergement'
+            }
+            
+        },(err, heberhascustom) =>{
+            console.log(heberhascustom);
+            if(err) cb(err, null)
+            else{
+                Offre.find({
+                    where:{
+                        hebergementId: heberhascustom.hebergementId
+                    },
+                    limit:1
+                },(err, offre) =>{
+                    console.log([heberhascustom, offre]);
+                    if(err) cb(err, null)
+                    else cb(null, [heberhascustom, offre])
+                })
+            }
+        })
+    }
+
+
+    Hebergementhascustomer.remoteMethod('liked', {
+        accepts: {arg: 'customerId', type: 'string'},
+        http:{ path: '/liked',verb:'get'},
+        returns: {type: 'object', root: true}
+  });
+
+
 
 };
