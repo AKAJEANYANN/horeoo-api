8904917fd88eb2c486c862c0b913112292cdd842
JEAN YANN AKA <akajeanyann@gmail.com>
2022-09-12 16:32:00 +0000

    update

--- server/boot/root.js
+++ server/boot/root.js
@@ -6,16 +6,37 @@
 'use strict';
 
 module.exports = function(server) {
-  server.dataSources.horeooAppDb.autoupdate();
+  // server.dataSources.horeooAppDb.autoupdate();
 
-  var ds = server.dataSources.horeooAppDb;
-  if(ds.autoupdate) {
-    ds.autoupdate();
-  } else {
-    ds.once('autoupdate', function() {
-      ds.autoupdate();
-    });
-  }
+  // var ds = server.dataSources.horeooAppDb;
+  // if(ds.connected) {
+  //   ds.autoupdate();
+  // } else {
+  //   ds.once('connected', function() {
+  //     ds.autoupdate();
+  //   });
+  // }
+
+
+
+  const appDataSources = [
+    app.dataSources.horeooAppDb
+  ]
+  
+  (async () => {
+    for (let i = 0; i < appDataSources.length; i++) {
+      const ds = appDataSources[ i ]
+  
+      if (ds.autoupdate) {
+        await new Promise((resolve, reject) => {
+          ds.autoupdate(err => {
+            if (err) return reject(err)
+            resolve()
+          })
+        })
+      }
+    }
+  })()
   
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
