46805f24e6dab2c5284c2d41590d8a6c67b82ec3
JEAN YANN AKA <akajeanyann@gmail.com>
2022-09-16 08:35:10 +0000

    update

--- common/models/hebergement.js
+++ common/models/hebergement.js
@@ -328,7 +328,7 @@ Hebergement.remoteMethod('approve',
                     approuveProvider: true,
                 }
             },(err, prov)=>{
-                if (prov !=[""]) 
+                if (prov.id !="") 
                 cb(null, hebergement)  
             })
             
