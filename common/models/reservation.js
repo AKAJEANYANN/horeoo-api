d1cbcd225fff32a013bf4c88a215b0b1debdb250
JEAN YANN AKA <akajeanyann@gmail.com>
2022-09-20 19:08:09 +0000

    update

--- common/models/reservation.js
+++ common/models/reservation.js
@@ -195,7 +195,7 @@ module.exports = function(Reservation) {
                             
                         })
     
-                    });
+                    })
 
                     cb(null, reservation)
                 })
