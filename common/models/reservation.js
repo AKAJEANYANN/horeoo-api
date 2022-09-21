229477cfa0c9d20dfc7bee490f04650ca5999e09
JEAN YANN AKA <akajeanyann@gmail.com>
2022-09-21 11:06:53 +0000

    update

--- common/models/reservation.js
+++ common/models/reservation.js
@@ -182,14 +182,22 @@ module.exports = function(Reservation) {
                                     "CUS"
                                     );
                             }
-                            else if(reservation.reservationEtat === "6" ){
+                            else if(reservation.reservationEtat === "6" && prod.device_fcm_token != ""){
 
                                 notify.sendPushNotification(
                                     prod.device_fcm_token,
-                                    reservations["customer"]["device_fcm_token"],
                                     "Réservation terminée",
                                     "Vous avez une réservation terminée !",
                                     "PRO"
+                                    );
+                            }
+                            else if(reservation.reservationEtat === "6" && reservations["customer"]["device_fcm_token"] != ""){
+
+                                notify.sendPushNotification(
+                                    reservations["customer"]["device_fcm_token"],
+                                    "Réservation terminée",
+                                    "Vous avez une réservation terminée !",
+                                    "CUS"
                                     );
                             }
                             
