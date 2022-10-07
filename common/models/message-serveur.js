9c2d40bd365f133df76a49d05c86a40e8beb1adb
JEAN YANN AKA <akajeanyann@gmail.com>
2022-10-07 03:15:45 +0000

    message server provider

--- common/models/message-serveur.js
+++ common/models/message-serveur.js
@@ -4,44 +4,44 @@ const notify = require("../../server/global/notify")
 module.exports = function(Messageserveur) {
 
     //messge de refus d'un provider
-    Messageserveur.refusProvider = function (req, idProvider, cb) {
+    // Messageserveur.refusProvider = function (req, idProvider, cb) {
         
-        var messageServeurBody = req.body.messageServeurBody; 
+    //     var messageServeurBody = req.body.messageServeurBody; 
 
-        const Provider = Messageserveur.app.models.provider;
+    //     const Provider = Messageserveur.app.models.provider;
 
-        Messageserveur.create({
-            objetMessage: messageServeurBody.objetMessage,
-            message: messageServeurBody.message,
-            providerId: idProvider
-        },
-            (err, mess) =>{
-                console.log(mess);
-                if(err) cb(err, null)
-                else{
-                    Provider.findById(idProvider,
-                        (err, pro) =>{
-                            notify.sendPushNotification(
-                                pro.device_fcm_token,
-                                "Refus d'approbation",
-                                "Votre compte fournisseur n'a pas été approuvé",
-                                "PRO"
-                                );
-                        });
+    //     Messageserveur.create({
+    //         objetMessage: messageServeurBody.objetMessage,
+    //         message: messageServeurBody.message,
+    //         providerId: idProvider
+    //     },
+    //         (err, mess) =>{
+    //             console.log(mess);
+    //             if(err) cb(err, null)
+    //             else{
+    //                 Provider.findById(idProvider,
+    //                     (err, pro) =>{
+    //                         notify.sendPushNotification(
+    //                             pro.device_fcm_token,
+    //                             "Refus d'approbation",
+    //                             "Votre compte fournisseur n'a pas été approuvé",
+    //                             "PRO"
+    //                             );
+    //                     });
 
-                        cb(null, mess);
-                }
+    //                     cb(null, mess);
+    //             }
 
-            })
-    }
+    //         })
+    // }
     
     
-    Messageserveur.remoteMethod('refusProvider',
-        {
-        accepts: [
-            { arg: 'req', type: 'object', 'http': {source: 'req'}},
-            { arg: 'idProvider', type: 'string' }],
-        http: { path: '/:idProvider/refusProvider', verb: 'post'},
-        returns : { type: 'object', root: true } 
-    });
+    // Messageserveur.remoteMethod('refusProvider',
+    //     {
+    //     accepts: [
+    //         { arg: 'req', type: 'object', 'http': {source: 'req'}},
+    //         { arg: 'idProvider', type: 'string' }],
+    //     http: { path: '/:idProvider/refusProvider', verb: 'post'},
+    //     returns : { type: 'object', root: true } 
+    // });
 };
