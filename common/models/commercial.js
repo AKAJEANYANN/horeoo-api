debfbbca824513a217479904827a8c522d0175d2
JEAN YANN AKA <akajeanyann@gmail.com>
2022-10-24 20:56:57 +0000

    update

--- common/models/commercial.js
+++ common/models/commercial.js
@@ -1,6 +1,79 @@
 'use strict';
+const notify = require("../../server/global/notify")
+
 
 module.exports = function(Commercial) {
+
+    Commercial.approuve = function (id, cb) {
+        
+        Commercial.findById(
+            id,
+            (err, commercial) =>{
+                commercial.updateAttributes({
+                    approuveCommercial: true,
+                    approval_datetime: Date.now()
+                },(err, commercial) =>{
+                    if(err) cb(err, null)
+                    else
+                        cb(null, commercial)
+
+                });
+
+                notify.sendPushNotification(
+                    commercial.device_fcm_token,
+                    "Commercial approuvé",
+                    "Votre compte commercial à été approuvé",
+                    "COM"
+                    );
+            })
+    }
+    
+    
+    Commercial.remoteMethod('approuve',
+    {
+        accepts: { arg: 'id', type: 'string' },
+        http: { path: '/:id/approuve', verb: 'post'},
+        returns : { type: 'object', root: true } 
+    });
+
+
+
+
+    Commercial.desapprouve = function (id, cb) {
+        
+        Commercial.findById(
+            id,
+            (err, commercial) =>{
+                commercial.updateAttributes({
+                    approuveCommercial: false,
+                    approval_datetime: Date.now()
+                },(err, commercial) =>{
+                    if(err) cb(err, null)
+                    else
+                        cb(null, commercial)
+
+                });
+
+                notify.sendPushNotification(
+                    commercial.device_fcm_token,
+                    "Commercial désapprouvé",
+                    "Votre compte commercial à été désapprouvé",
+                    "COM"
+                    );
+            })
+    }
+    
+    
+    Commercial.remoteMethod('desapprouve',
+    {
+        accepts: { arg: 'id', type: 'string' },
+        http: { path: '/:id/desapprouve', verb: 'post'},
+        returns : { type: 'object', root: true } 
+    });
+
+
+
+
     Commercial.approve = function (id, cb) {
         Commercial.findById(
             id,
