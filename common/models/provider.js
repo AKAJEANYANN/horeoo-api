ecdb5fc8bad61a88d93f4c57441b51a376f5f2b9
JEAN YANN AKA <akajeanyann@gmail.com>
2022-10-24 22:57:08 +0000

    update

--- common/models/provider.js
+++ common/models/provider.js
@@ -478,7 +478,7 @@ module.exports = function(Provider) {
                 notify.sendPushNotification(
                     provider.device_fcm_token,
                     "Fournisseur désactivé",
-                    "Votre compte fournisseur à été désactivé pour méchanceté d'ecouteur Iphone",
+                    "Votre compte fournisseur a été désactivé pour non respect de nos règle !",
                     "PRO"
                     );
             })
