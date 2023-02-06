8d0901f499be4b66419f99067b253b36deae36ff
JEAN YANN AKA <akajeanyann@gmail.com>
2023-02-06 22:19:02 +0000

    update

--- common/models/equipement.js
+++ common/models/equipement.js
@@ -2,4 +2,54 @@
 
 module.exports = function(Equipement) {
 
+    Equipement.delete = function (id, cb) {
+        Equipement.findById(
+            id,
+            (err, equipement) =>{
+                if(equipement){
+                equipement.delete((err, he)=>{
+                    cb(null, 'Equipement supprimé')
+                })
+            }
+            })
+    }
+    
+    
+    Equipement.remoteMethod('delete',
+    {
+        accepts: { arg: 'id', type: 'string' },
+        http: { path: '/:id/delete', verb: 'post'},
+        returns : { type: 'object', root: true } 
+    });
+
+
+
+
+    Equipement.actif = function (id, cb) {
+
+        Equipement.findById(
+            id,
+            (err, equipement) =>{
+
+                console.log(equipement)
+                if (equipement) {
+                    equipement.updateAttributes({
+                        actifEquipement: true,
+                    },(err, equipement) =>{
+                        
+                        cb(null, equipement)
+                    })
+                }
+                
+            })
+    }
+    
+    
+    Equipement.remoteMethod('actif',
+    {
+        accepts: { arg: 'id', type: 'string' },
+        http: { path: '/:id/actif', verb: 'post'},
+        returns : { type: 'object', root: true } 
+    });
+
 };
