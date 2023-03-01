c5f9d373973edd9a85714a6f8baf704ecb65291d
JEAN YANN AKA <akajeanyann@gmail.com>
2023-03-01 11:56:53 +0000

    update

--- common/models/message-serveur.js
+++ common/models/message-serveur.js
@@ -1,4 +1,5 @@
 'use strict';
+const notify = require("../../server/global/notify")
 
 module.exports = function(Messageserveur) {
 
@@ -46,6 +47,60 @@ module.exports = function(Messageserveur) {
             { arg: 'req', type: 'object', 'http': {source: 'req'}},
         ],
         http: { path: '/postMessage', verb: 'post'},
+        returns : { type: 'object', root: true } 
+    });
+
+
+
+
+
+
+    // envoie message a un provider pour non approbation de son hebergement
+    Messageserveur.postMessageHeberge= function (req, cb) {
+
+        const Provider = Messageserveur.app.models.provider;
+        
+        var objetMessage= req.body.objetMessage;
+        var message= req.body.message;
+        var providerId= req.body.providerId;
+        
+        Messageserveur.create({
+            objetMessage: objetMessage,
+            message: message,
+            providerId: providerId,
+        },(err, message) =>{
+            if(err) cb(err, null);
+            else{
+                Provider.findOne({
+                    where:{
+                        id: message.providerId
+                    }
+                },(err, provider) =>{
+                        
+                    notify.sendPushNotification(
+                        provider.device_fcm_token,
+                        "Hébergement non approuvé",
+                        "Votre hébergement n'a pas été approuvé",
+                        "PRO"
+                        );
+                    }
+                    
+                );
+
+                cb(null, message);
+            }
+        })
+        
+    }
+
+
+
+    Messageserveur.remoteMethod('postMessageHeberge',
+    {
+        accepts: [
+            { arg: 'req', type: 'object', 'http': {source: 'req'}},
+        ],
+        http: { path: '/postMessageHeberge', verb: 'post'},
         returns : { type: 'object', root: true } 
     });
 
